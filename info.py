c304d9e2a3fa5156685699ddeb680b3cc8277ae7
Yaseen270 <bdgamesbazar71@gmail.com>
2025-07-27 18:27:26 +0600

    Update info.py

--- info.py
+++ info.py
@@ -75,7 +75,7 @@ YEARS = [f'{i}' for i in range(2025, 2002,-1 )]
 SEASONS = [f'season {i}'for i in range (1 , 23)]
 
 # Pictures And Reaction
-START_IMG = (environ.get('START_IMG', 'https://envs.sh/SpT.jpg https://envs.sh/Spn.jpg')).split()
+START_IMG = (environ.get('START_IMG', 'https://i.postimg.cc/rsdvdr0X/IMG-20250727-165359-560.jpg https://i.postimg.cc/k5fZfcWP/IMG-20250727-165354-492.jpg')).split()
 FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
 REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
 PAYPICS = (environ.get('PAYPICS', 'https://graph.org/file/f4db1c3ad3d9e38b328e6.jpg')).split()
